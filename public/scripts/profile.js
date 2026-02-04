00905ed641f2ef60be9078124bfd4253b2da3923
= <=>
2026-02-04 12:38:36 -0500

    Replace promise/callback with async/await

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,7 +1,18 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const ProfileContainerElementId = 'profile-container';
 const NotFoundRoute = '/404.html';
-document.addEventListener('DOMContentLoaded', () => {
+const RosterDataRoute = '/public/data/roster.json';
+const ErrorHTML = '<div>Something went wrong and this page cannot be loaded.</div>';
+document.addEventListener('DOMContentLoaded', () => __awaiter(void 0, void 0, void 0, function* () {
     // Extract member ID from URL
     const urlParams = new URLSearchParams(window.location.search);
     const memberId = urlParams.get('id');
@@ -15,20 +26,24 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.replace(NotFoundRoute);
         return;
     }
-    fetch('/public/data/roster.json')
-        .then(response => response.json())
-        .then((data) => {
-        const member = data.find((m) => m.uaNetId === memberId);
-        if (member) {
-            profileContainer.innerHTML = `
-                    <div class="roster-card">
-                        <img src="${member.pfp || '/public/assets/defaults/default_zipcode_pfp.svg'}" alt="Profile Picture" class="pfp">
-                        <div class="name">${member.name}</div>
-                        <div class="title">${member.title || 'Member'}</div>
-                        <div class="major">Major: ${member.major}</div>
-                        <p>Graduation Year: ${member.graduationYear}</p>
-                    </div>
-                `;
-        }
-    });
-});
+    // Fetch roster data
+    const response = yield fetch(RosterDataRoute);
+    if (!response.ok) {
+        profileContainer.innerHTML = ErrorHTML;
+        return;
+    }
+    const rosterData = yield response.json();
+    // Retrieve member profile
+    const member = rosterData.find((m) => m.uaNetId === memberId);
+    if (member) {
+        profileContainer.innerHTML = `
+            <div class="roster-card">
+                <img src="${member.pfp || '/public/assets/defaults/default_zipcode_pfp.svg'}" alt="Profile Picture" class="pfp">
+                <div class="name">${member.name}</div>
+                <div class="title">${member.title || 'Member'}</div>
+                <div class="major">Major: ${member.major}</div>
+                <p>Graduation Year: ${member.graduationYear}</p>
+            </div>
+        `;
+    }
+}));
--- src/profile.ts
+++ src/profile.ts
@@ -10,8 +10,10 @@ type ZipCoder = {
 
 const ProfileContainerElementId = 'profile-container' as const;
 const NotFoundRoute = '/404.html' as const;
+const RosterDataRoute = '/public/data/roster.json' as const;
+const ErrorHTML = '<div>Something went wrong and this page cannot be loaded.</div>' as const;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
 
     // Extract member ID from URL
     const urlParams = new URLSearchParams(window.location.search);
@@ -28,21 +30,26 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
-    fetch('/public/data/roster.json')
-        .then(response => response.json())
-        .then((data: ZipCoder[]) => {
-            const member = data.find((m: ZipCoder) => m.uaNetId === memberId);
-            if (member) {
-                profileContainer.innerHTML = `
-                    <div class="roster-card">
-                        <img src="${member.pfp || '/public/assets/defaults/default_zipcode_pfp.svg'}" alt="Profile Picture" class="pfp">
-                        <div class="name">${member.name}</div>
-                        <div class="title">${member.title || 'Member'}</div>
-                        <div class="major">Major: ${member.major}</div>
-                        <p>Graduation Year: ${member.graduationYear}</p>
-                    </div>
-                `;
-            }
-        });
+    // Fetch roster data
+    const response = await fetch(RosterDataRoute);
+    if (!response.ok) {
+        profileContainer.innerHTML = ErrorHTML;
+        return;
+    }
+    const rosterData = await response.json() as ZipCoder[];
+
+    // Retrieve member profile
+    const member = rosterData.find((m: ZipCoder) => m.uaNetId === memberId);
+    if (member) {
+        profileContainer.innerHTML = `
+            <div class="roster-card">
+                <img src="${member.pfp || '/public/assets/defaults/default_zipcode_pfp.svg'}" alt="Profile Picture" class="pfp">
+                <div class="name">${member.name}</div>
+                <div class="title">${member.title || 'Member'}</div>
+                <div class="major">Major: ${member.major}</div>
+                <p>Graduation Year: ${member.graduationYear}</p>
+            </div>
+        `;
+    }
 
 });
