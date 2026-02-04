dfba3e21c44ea501637d19159789a5c82abbce10
= <=>
2026-02-04 12:13:40 -0500

    Replace nested condition with guard clause

--- src/profile.ts
+++ src/profile.ts
@@ -12,25 +12,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const memberId = urlParams.get('id');
 
-    if (memberId) {
-        fetch('/public/data/roster.json')
-            .then(response => response.json())
-            .then((data: ZipCoder[]) => {
-                const member = data.find((m: ZipCoder) => m.uaNetId === memberId);
-                if (member) {
-                    const profileContainer = document.getElementById('profile-container');
-                    if (profileContainer) {
-                        profileContainer.innerHTML = `
-                            <div class="roster-card">
-                                <img src="${member.pfp || '/public/assets/defaults/default_zipcode_pfp.svg'}" alt="Profile Picture" class="pfp">
-                                <div class="name">${member.name}</div>
-                                <div class="title">${member.title || 'Member'}</div>
-                                <div class="major">Major: ${member.major}</div>
-                                <p>Graduation Year: ${member.graduationYear}</p>
-                            </div>
-                        `;
-                    }
+    if (!memberId) window.location.replace('/404.html')
+
+    fetch('/public/data/roster.json')
+        .then(response => response.json())
+        .then((data: ZipCoder[]) => {
+            const member = data.find((m: ZipCoder) => m.uaNetId === memberId);
+            if (member) {
+                const profileContainer = document.getElementById('profile-container');
+                if (profileContainer) {
+                    profileContainer.innerHTML = `
+                        <div class="roster-card">
+                            <img src="${member.pfp || '/public/assets/defaults/default_zipcode_pfp.svg'}" alt="Profile Picture" class="pfp">
+                            <div class="name">${member.name}</div>
+                            <div class="title">${member.title || 'Member'}</div>
+                            <div class="major">Major: ${member.major}</div>
+                            <p>Graduation Year: ${member.graduationYear}</p>
+                        </div>
+                    `;
                 }
-            });
-    }
+            }
+        });
+
 });
