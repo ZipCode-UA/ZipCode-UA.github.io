97e7f3ed016cb91e5512fbc3cda625ae5d1ddad7
Lucas Ergh <LucasErgh@gmail.com>
2026-02-24 18:02:17 -0500

    Update leaderboards.js file

--- public/scripts/leaderboards.js
+++ public/scripts/leaderboards.js
@@ -2,16 +2,18 @@
 fetch('/public/data/leaderboards.json')
     .then(response => response.json())
     .then((data) => {
-    const calendarContainer = document.getElementById('leaderboards-container');
-    if (calendarContainer) {
-        data.forEach(item => {
-            const eventDiv = document.createElement('div');
-            eventDiv.innerHTML = `
-                    <h3>${item.date}</h3>
-                    <p>${item.game}</p>
-                    <p>${item.winner}</p>
-                `;
-            calendarContainer.appendChild(eventDiv);
-        });
-    }
+    const leaderboardContainer = document.getElementById('leaderboards-container');
+    leaderboard = "<table boarder='1'><tr><th>Date</th><th>Game</th><th>Winner</th></table>"
+    //if (calendarContainer) {
+    //    data.forEach(item => {
+    //        const eventDiv = document.createElement('div');
+    //        eventDiv.innerHTML = `
+    //                <h3>${item.date}</h3>
+    //                <p>${item.game}</p>
+    //                <p>${item.winner}</p>
+    //            `;
+    //        calendarContainer.appendChild(eventDiv);
+    //    });
+    //}
+    leaderboardContainer.innerHTML = leaderboard;
 });
